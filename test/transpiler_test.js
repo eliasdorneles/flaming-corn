100e4d2670b0ed4d2d50bf7bdbcd1387e81b61fd
Elias Dorneles <eliasdorneles@gmail.com>
2015-01-11 21:46:17 -0200

    simplistic if statement, some refactoring in tests

--- test/transpiler_test.js
+++ test/transpiler_test.js
@@ -1,16 +1,30 @@
 'use strict';
 
 var transpiler = require('../transpiler.js');
+var utils = transpiler.utils;
 
 exports.simple_transpiling = {
-    simple_assignment: function(t) {
-        t.equals("a = 'oi'\n", transpiler.transpile("var a = 'oi';"));
-        t.equals("hello = \"hi!\"\n", transpiler.transpile("var hello = \"hi!\";"));
-        t.equals("some_variable = 1\n", transpiler.transpile("var some_variable = 1"));
-        t.done();
+    simple_assignment: function(assert) {
+        assert.equals("a = 'oi'\n", transpiler.transpile("var a = 'oi';"));
+        assert.equals("hello = \"hi!\"\n", transpiler.transpile("var hello = \"hi!\";"));
+        assert.equals("some_variable = 1\n", transpiler.transpile("var some_variable = 1"));
+        assert.done();
     },
-    multiple_assignment: function(t) {
-        t.equals("a = 'oi'\nb = 13\n", transpiler.transpile("var a = 'oi', b = 13;"));
-        t.done();
+    multiple_assignment: function(assert) {
+        assert.equals("a = 'oi'\nb = 13\n", transpiler.transpile("var a = 'oi', b = 13;"));
+        assert.done();
     },
+    simple_conditional: function(assert) {
+        assert.equals("if 1:\n    a = 1\n", transpiler.transpile("if (1) a = 1;"))
+        assert.done();
+    },
+}
+
+exports.util_functions = {
+    indent: function(assert) {
+        assert.equals('', utils.indent(0));
+        assert.equals('    ', utils.indent(1));
+        assert.equals('        ', utils.indent(2));
+        assert.done();
+    }
 }
--- transpiler.js
+++ transpiler.js
@@ -10,6 +10,14 @@ var walker = require('./node_modules/acorn/util/walk');
 var acorn_options = {
 }
 
+function repeat(s, num) {
+    return new Array(num + 1).join(s);
+}
+
+function indent(level) {
+    return repeat('    ', level);
+}
+
 // Node objects:
 // https://developer.mozilla.org/en-US/docs/Mozilla/Projects/SpiderMonkey/Parser_API#Node_objects
 var visitors = {
@@ -26,15 +34,36 @@ var visitors = {
                 // console.log("DECLARATION:", decl);
                 state.result += decl.id.name + " = " + decl.init.raw + "\n";
             });
+        } else if (node.type == 'IfStatement') {
+            state.result += "if " + node.test.raw + ":\n";
+            state.level += 1;
+
+            // TODO: this should be handled separately as assignment expression,
+            // but we need to change the walker strategy before that
+            var assign_expr = node.consequent.expression;
+            state.result += indent(state.level) + assign_expr.left.name + " = " + assign_expr.right.raw + "\n";
+
+            state.level -= 1;
         }
-    }
+    },
+    // ExpressionStatement: function(node, state) {
+    //     console.log("EXPR_STMT: ", node);
+    // },
+    // AssignmentExpression: function(node, state) {
+    //     state.result += indent(state.level) + node.left.name + " = " + node.right.raw + "\n";
+    // }
 }
 
 exports.transpile = function(js_code){
     var ast = parser.parse(js_code, acorn_options);
     var state = {
         result: '',
+        level: 0,
     }
     walker.simple(ast, visitors, undefined, state);
     return state.result;
+}
+
+exports.utils = {
+    indent: indent,
 }
